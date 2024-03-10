d40933ba21987081f6eeb01595488d4ca0b079d6
ducphamle2 <ducphamle212@gmail.com>
2024-03-09 18:26:12 -0800

    feat: add alert confirmed checkpoints logic

--- src/alert-bot.ts
+++ src/alert-bot.ts
@@ -5,6 +5,10 @@ import { WebhookClient } from "discord.js";
 const checkpointFeesInterval = 5; // 3 hours
 const sleepTime = 60000; // 1 mins
 
+const milliToHour = (timestamp: number) => {
+  return timestamp / 1000 / 60 / 60;
+};
+
 const detectCheckpointNotEnoughFees = async (currentIndex: number) => {
   const currentCheckpointFees = await fetch(
     `${networkConfig.LCD}/bitcoin/checkpoint_fee_info?checkpoint_index=${currentIndex}`
@@ -24,8 +28,37 @@ const detectCheckpointNotEnoughFees = async (currentIndex: number) => {
   };
 };
 
-const milliToHour = (timestamp: number) => {
-  return timestamp / 1000 / 60 / 60;
+const alertConfirmedCheckpoints = async (prevLastConfirmedIndex: number) => {
+  const currentLastConfirmedIndex = await fetch(
+    `${networkConfig.LCD}/bitcoin/checkpoint/last_confirmed_index`
+  ).then((data) => data.json());
+  if (
+    !currentLastConfirmedIndex ||
+    !currentLastConfirmedIndex.last_confirmed_index
+  ) {
+    throw `Cannot query last checkpoint confirmed index ${JSON.stringify(
+      serializeError(currentLastConfirmedIndex)
+    )}`;
+  }
+  // if it is the first time we call this func -> just return empty [] & the last confirmed index
+  if (prevLastConfirmedIndex === -1)
+    return {
+      listNewConfirmedIndexes: [],
+      prevLastConfirmedIndex: currentLastConfirmedIndex.last_confirmed_index,
+    };
+  let listNewConfirmedIndexes: number[] = [];
+  // collect next confirmed indexes that we have not alerted yet
+  for (
+    let i = prevLastConfirmedIndex + 1;
+    i <= currentLastConfirmedIndex;
+    i++
+  ) {
+    listNewConfirmedIndexes.push(i);
+  }
+  return {
+    listNewConfirmedIndexes,
+    prevLastConfirmedIndex: currentLastConfirmedIndex.last_confirmed_index,
+  };
 };
 
 const main = async () => {
@@ -33,14 +66,15 @@ const main = async () => {
   const webhookClient = new WebhookClient({
     url: userConfig.DISCORD_WEBHOOK_URL as string,
   });
+  let prevLastConfirmedIndex = -1;
 
   while (true) {
     try {
+      // detect checkpoint not enough fees logic
       const curSigSet = await fetch(`${networkConfig.RELAYERS[0]}/sigset`).then(
         (data) => data.json()
       );
       const curIndex: number = curSigSet.index;
-      // if there is a problem with
       if (!curIndex) {
         throw "Error querying current checkpoint index";
       }
@@ -55,8 +89,23 @@ const main = async () => {
         ) {
           // reset checkpoint time
           checkpointNotEnoughFeesIntervalHour = nowInHour;
-          throw `Cannot relay deposit and withdraw tokens due to not enough miner fees. Wanted ${minerFees}, got ${feeCollected}`;
+          throw `Cannot confirm checkpoint ${curIndex} & relay deposit and withdraw tokens due to not enough miner fees. Wanted ${minerFees}, got ${feeCollected}`;
         }
+      }
+
+      // alert confirmed checkpoints logic
+      const {
+        listNewConfirmedIndexes,
+        prevLastConfirmedIndex: newLastConfirmedIndex,
+      } = await alertConfirmedCheckpoints(prevLastConfirmedIndex);
+      prevLastConfirmedIndex = newLastConfirmedIndex;
+      console.log({ listNewConfirmedIndexes, prevLastConfirmedIndex });
+      if (listNewConfirmedIndexes.length > 0) {
+        await webhookClient.send(
+          `Checkpoints ${listNewConfirmedIndexes.join(
+            ","
+          )} have been confirmed on Bitcoin`
+        );
       }
     } catch (error) {
       console.log("error: ", error);
